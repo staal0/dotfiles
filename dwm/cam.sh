252dc61d685ebe2880f9a899f079f899b56af38c
Dennis Staal <dennis@staal-mail.dk>
2021-03-04 11:22:04 +0100

    Added appimage, shutdown dmenu and calcurse scripts

--- dwm/cam.sh
+++ dwm/cam.sh
@@ -3,15 +3,15 @@ if [ $# -gt 0 ]
 then
   if [ $1 = 'all' ]
   then
-    mplayer -monitoraspect 16:9 -fs -title "cam 1" -fps 20 -demuxer lavf "https://URL/zm/cgi-bin/nph-zms?mode=jpeg&monitor=1&scale=100&maxfps=25&buffer=1000&user=USERNAME&pass=PASSWORD" &
-    mplayer -monitoraspect 16:9 -fs -title "cam 2" -fps 20 -demuxer lavf "https://URL/zm/cgi-bin/nph-zms?mode=jpeg&monitor=2&scale=100&maxfps=25&buffer=1000&user=USERNAME&pass=PASSWORD" &
-    mplayer -monitoraspect 16:9 -fs -fs -title "cam 3" -fps 20 -demuxer lavf "https://URL/zm/cgi-bin/nph-zms?mode=jpeg&monitor=3&scale=100&maxfps=25&buffer=1000&user=USERNAME&pass=PASSWORD"
-    #mplayer -vf cropdetect -monitoraspect 16:9 -fs -title "cam 1" rtsp://USERNAME:PASSWORD@URL:7002/live/1 &
-    #mplayer -vf cropdetect -monitoraspect 16:9 -fs -title "cam 2" rtsp://USERNAME:PASSWORD@URL:7002/live/2 &
-    #mplayer -vf cropdetect -monitoraspect 16:9 -fs -title "cam 3" rtsp://USERNAME:PASSWORD@URL:7002/live/4
+    mpv -monitoraspect 16:9 -fs -title "cam 1" -fps 20 -demuxer lavf "https://URL/zm/cgi-bin/nph-zms?mode=jpeg&monitor=1&scale=100&maxfps=25&buffer=1000&user=USERNAME&pass=PASSWORD" &
+    mpv -monitoraspect 16:9 -fs -title "cam 2" -fps 20 -demuxer lavf "https://URL/zm/cgi-bin/nph-zms?mode=jpeg&monitor=2&scale=100&maxfps=25&buffer=1000&user=USERNAME&pass=PASSWORD" &
+    mpv -monitoraspect 16:9 -fs -fs -title "cam 3" -fps 20 -demuxer lavf "https://URL/zm/cgi-bin/nph-zms?mode=jpeg&monitor=3&scale=100&maxfps=25&buffer=1000&user=USERNAME&pass=PASSWORD"
+    #mpv -vf cropdetect -monitoraspect 16:9 -fs -title "cam 1" rtsp://USERNAME:PASSWORD@URL:7002/live/1 &
+    #mpv -vf cropdetect -monitoraspect 16:9 -fs -title "cam 2" rtsp://USERNAME:PASSWORD@URL:7002/live/2 &
+    #mpv -vf cropdetect -monitoraspect 16:9 -fs -title "cam 3" rtsp://USERNAME:PASSWORD@URL:7002/live/4
   else
-    mplayer -monitoraspect 16:9 -fs -title "cam$1" -fps 20 -demuxer lavf "https://URL/zm/cgi-bin/nph-zms?mode=jpeg&monitor=$1&scale=100&maxfps=25&buffer=1000&user=USERNAME&pass=PASSWORD"
-    #mplayer -vf cropdetect -monitoraspect 16:9 -fs -title "cam$1" rtsp://USERNAME:PASSWORD@URL:7002/live/$1
+    mpv -monitoraspect 16:9 -fs -title "cam$1" -fps 20 -demuxer lavf "https://URL/zm/cgi-bin/nph-zms?mode=jpeg&monitor=$1&scale=100&maxfps=25&buffer=1000&user=USERNAME&pass=PASSWORD"
+    #mpv -vf cropdetect -monitoraspect 16:9 -fs -title "cam$1" rtsp://USERNAME:PASSWORD@URL:7002/live/$1
   fi
 else
 echo 'choose cam id'
--- dwm/feh_slideshow.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-feh -Y -x -q -D 10 -B black -z -r --keep-zoom-vp --auto-rotate ~/picture_folder
