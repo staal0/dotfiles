6f569953cf75900aa1af756908e7743eba8477c8
Dennis Staal <dennis@staal-mail.dk>
2021-07-14 09:00:02 +0200

    Renamed dmenu shutdown script and added logout

--- dwm/dmenu_session.sh
+++ dwm/dmenu_session.sh
@@ -5,9 +5,10 @@
 ###
 
 DMENU='dmenu -l 20 -nf #f8f8f2 -nb #20212b -sf #f8f8f2 -sb #44475a'
-choice=$(echo -e "shutdown\nreboot" | $DMENU)
+choice=$(echo -e "shutdown\nreboot\nlogout" | $DMENU)
 
 case "$choice" in
-  shutdown) sudo shutdown -h now & ;;
-  reboot) sudo shutdown -r now & ;;
+  shutdown) shutdown -h now & ;;
+  reboot) shutdown -r now & ;;
+  logout) pkill dwm & ;;
 esac
