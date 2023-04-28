1dc75500e8fb204fe59a0f0c96bdbbd424a5bc01
Dennis Staal <dennis@staal-mail.dk>
2023-04-28 12:41:24 +0200

    Updating DWM status bar

--- .dwm/dwm_status.sh
+++ .dwm/dwm_status.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
-# Network speed stuff stolen from http://linuxclues.blogspot.sg/2009/11/shell-script-show-network-speed.html
 
-# This function parses /proc/net/dev file searching for a line containing $interface data.
-# Within that line, the first and ninth numbers after ':' are respectively the received and transmited bytes.
-function get_bytes {
-# Find active network interface
-interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
-line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
-eval $line
-now=$(date +%s%N)
-}
-
-# Function which calculates the speed using actual and old byte number.
-# Speed is shown in KByte per second when greater or equal than 1 KByte per second.
-# This function should be called each second.
-
-function get_velocity {
-value=$1
-old_value=$2
-now=$3
-
-timediff=$(($now - $old_time))
-velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
-if test "$velKB" -gt 1024
-then
-	echo $(echo "scale=2; $velKB/1024" | bc)MB/s
-else
-	echo ${velKB}KB/s
-fi
-}
-
-# Get initial values
-get_bytes
-old_received_bytes=$received_bytes
-old_transmitted_bytes=$transmitted_bytes
-old_time=$now
+# PIPEWIRE
+#print_volume() {
+#        status="$(pw-volume status | jq -r '.tooltip')"
+#        if test "$status" == "muted"
+#        then
+#        	echo -e "${status} vol"
+#        else
+#                volume="$(pw-volume status | jq -r '.percentage')"
+#        	echo -e "${volume}% vol"
+#        fi
+#}
 
 print_volume() {
 	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
@@ -53,47 +29,37 @@ print_volume() {
 	fi
 }
 
-#print_wifi() {
-#	ip=$(ip route get 8.8.8.8 2>/dev/null|grep -Eo 'src [0-9.]+'|grep -Eo '[0-9.]+')
-#
-#	if=wlan0
-#		while IFS=$': \t' read -r label value
-#		do
-#			case $label in SSID) SSID=$value
-#				;;
-#			signal) SIGNAL=$value
-#				;;
-#		esac
-#	done < <(iw "$if" link)
-#
-#	echo -e "$SSID $SIGNAL $ip"
-#}
-
 print_mem(){
-	memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
-	echo -e "$memfree"
+	# memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
+	# echo -e "$memfree"
+	mem="$(free | grep Mem | awk '{print $3/$2 * 100.0}')"
+	printf "%.1f" $mem
+	echo "% mem"
 }
 
-#print_temp(){
-#	test -f /sys/class/thermal/thermal_zone0/temp || return 0
-#	echo $(head -c 2 /sys/class/thermal/thermal_zone0/temp)C
-#}
+print_cpu(){
+	cpu=$(top -bn1 | grep "Cpu(s)" | \
+           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+           awk '{print 100 - $1"%"}')
 
-#print_bat(){
-#	hash acpi || return 0
-#	onl="$(grep "on-line" <(acpi -V))"
-#	charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)"
-#	if test -z "$onl"
-#	then
+	echo "$cpu cpu"
+}
+
+print_battery(){
+	hash acpi || return 0
+	onl="$(grep "on-line" <(acpi -V))"
+	charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/CMB1/capacity)"
+	if test -z "$onl"
+	then
 		# suspend when we close the lid
 		#systemctl --user stop inhibit-lid-sleep-on-battery.service
-#		echo -e "${charge}"
-#	else
+		echo -e "${charge}% bat"
+	else
 		# On mains! no need to suspend
 		#systemctl --user start inhibit-lid-sleep-on-battery.service
-#		echo -e "${charge}"
-#	fi
-#}
+		echo -e "${charge}% psu"
+	fi
+}
 
 print_date(){
 	date "+%a (%V) %d-%m-%Y %X"
@@ -101,21 +67,7 @@ print_date(){
 
 while true
 do
-
-	# Get new transmitted, received byte number values and current time
-	get_bytes
-
-	# Calculates speeds
-	vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
-	vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
-
-	xsetroot -name "$(print_mem)MB mem | $vel_recv / $vel_trans | $(print_volume) | $(print_date)"
-
-	# Update old values to perform new calculations
-	old_received_bytes=$received_bytes
-	old_transmitted_bytes=$transmitted_bytes
-	old_time=$now
-
+	xsetroot -name "$(print_cpu) | $(print_mem) | $(print_volume) | $(print_date)"
 	sleep 1
-
 done
+
