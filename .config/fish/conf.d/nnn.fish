4c2d4b0f5bfe94f248ce1f30ac7d07f860ec5eea
Dennis Staal <dennis@staal-mail.dk>
2024-03-30 11:45:46 +0100

    Added nnn config and plugins

---
+++ .config/fish/conf.d/nnn.fish
@@ -0,0 +1,6 @@
+alias nnn='nnn -e'
+export NNN_FCOLORS="0B0B04060006060009060B06"
+export NNN_FIFO='/tmp/nnn.fifo'
+export NNN_PAGER='bat'
+export NNN_TERMINAL='alacritty --title nnn-preview'
+export NNN_PLUG='f:finder;o:fzopen;p:preview-tui;d:diffs;v:imgview'
