572add9988d354d8a50e595da22b9867697cb6d5
Dennis Staal <dennis@staal-mail.dk>
2024-07-09 00:40:16 +0200

    Switched to Sway and Catppuccin

--- .config/fish/conf.d/nnn.fish
+++ .config/fish/conf.d/nnn.fish
@@ -1,5 +1,11 @@
 alias nnn='n'
-export NNN_FCOLORS="0B0B04060006060009060B06"
+# Catppuccin color scheme
+# Export Context Colors
+export NNN_COLORS="#04020301;4231"
+
+# Finally Export the set file colors ( Both options require this)
+export NNN_FCOLORS="030304020705050801060301"
+
 export NNN_FIFO='/tmp/nnn.fifo'
 export NNN_PAGER='bat'
 export NNN_TERMINAL='alacritty --title nnn-preview'
--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -100,15 +100,12 @@ config.bind('e', 'set-cmd-text -s :open {url:pretty}')
 config.bind('<Alt-Shift-u>', 'spawn --userscript qute-keepassxc', mode='insert')
 config.bind('pw', 'spawn --userscript qute-keepassxc', mode='normal')
 
-# DRACULA THEME
-import dracula.draw
+import catppuccin
 
-# Load existing settings made via :set
+# load your autoconfig, use this, if the rest of your config is empty!
 config.load_autoconfig()
 
-dracula.draw.blood(c, {
-    'spacing': {
-        'vertical': 6,
-        'horizontal': 8
-    }
-})
+# set the flavor you'd like to use
+# valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
+# last argument (optional, default is False): enable the plain look for the menu rows
+catppuccin.setup(c, 'mocha', True)
