7d1607231a219d291abbe2091fe89978cc09e776
Dennis Staal <dennis@staal-mail.dk>
2024-11-24 11:12:10 +0100

    Added gruvbox

--- config/sway/appimage.sh
+++ config/sway/appimage.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+uifont="monospace 17"
+bg="#282828"
+bg0_h="#1d2021"
+bg1="#3c3836"
+bg2="#504945"
+bg3="#665c54"
+fg="#ebdbb2"
+red="#cc241d"
+blue="#458588"
+purple="#b16286"
+yellow="#d79921"
+
 cd ~/applications
 prog="$(ls *.AppImage)"
-#cmd=$(bemenu  -l 20 --nf '#f8f8f2' --nb '#20212b' --sf '#f8f8f2' --sb '#44475a' -p 'Launch AppImage'   <<< "$prog")
-uifont="monospace 17"
-prompt="#ffffff"
-highlight="#3daee9"
-cmd=$(bemenu -i -l 20 --fn "$uifont" -p 'Launch AppImage' -B 1 --tf $prompt --hf $highlight --sf $highlight --scf $highlight --bdr $highlight <<< "$prog")
+cmd=$(bemenu -c -W 0.25 -i -l 20 --fn "$uifont" -p 'appimage' -B 1 --tf "$red" --hf "$yellow" --sf "$blue" --scf "$blue" --bdr "$purple" <<< "$prog")
 
 case ${cmd%% *} in
 	*)	exec "~/applications/${cmd}"  ;;
--- config/sway/powermenu.sh
+++ config/sway/powermenu.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 uifont="monospace 17"
-prompt="#ffffff"
-highlight="#3daee9"
-choice=$(echo -e "shutdown\nreboot\nlogout" | bemenu -l 20 -p "Select option:" --fn "$uifont" -B 1 --tf $prompt --hf $highlight --sf $highlight --scf $highlight --bdr $highlight)
+bg="#282828"
+bg0_h="#1d2021"
+bg1="#3c3836"
+bg2="#504945"
+bg3="#665c54"
+fg="#ebdbb2"
+red="#cc241d"
+blue="#458588"
+purple="#b16286"
+yellow="#d79921"
+
+choice=$(echo -e "shutdown\nreboot\nlogout" | bemenu -c -W 0.25 -i -l 20 -p "power menu:" -B 1 --fn "$uifont" --tf "$red" --hf "$yellow" --sf "$blue" --scf "$blue" --bdr "$purple")
 
 case "$choice" in
-  shutdown) alacritty -e paru && shutdown -h now & ;;
-  reboot) alacritty -e paru && shutdown -r now & ;;
-  logout) pkill sway & ;;
+  shutdown) alacritty -e sudo pacman -Syu && shutdown -h now & ;;
+  reboot) alacritty -e sudo pacman -Syu && shutdown -r now & ;;
+  logout) swaymsg exit & ;;
 esac
